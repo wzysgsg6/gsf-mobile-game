21ccdee7984753c19a6d2c389c089a858417113d
wzysgsg6 <ziwang324@gmail.com>
2026-09-19 23:23:40 +0800

    Implement printable question card (printBtn was a dead button)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='gsf-pre3-v47';
+const CACHE='gsf-pre3-v48';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./icon-maskable-512.png','./apple-touch-icon.png','./knowledge-points.js','./knowledge-points.json','./laws.js','./laws.json','./questions.js','./questions.json'])));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{
